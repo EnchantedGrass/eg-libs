724855bfef2f8e6db1fed0cd997740929d18fe46
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-11-30 19:12:28 +0700

    refactor: add key property as backing field

--- core/src/top/enchantedgrass/egLibs/registry/Registrable.kt
+++ core/src/top/enchantedgrass/egLibs/registry/Registrable.kt
@@ -1,11 +1,19 @@
 package top.enchantedgrass.egLibs.registry
 
+import net.kyori.adventure.key.Key
 import net.kyori.adventure.key.Keyed
 
 /**
  * Represents an object that can be registered to a [Registry].
  */
 interface Registrable : Keyed {
+    /**
+     * The key of this object.
+     */
+    val key: Key
+
+    override fun key(): Key = key
+
     /**
      * Invokes when this object is registered to a [Registry].
      */
