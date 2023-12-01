41a1c799e272037669aa71e9459628697e7afd8e
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-12-01 07:13:52 +0700

    build(config): change deps configuration

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
 }
 
 dependencies {
-    implementation(projects.egLibsRegistry)
-    implementation(platform(libs.jackson.bom))
-    implementation(libs.jackson.module.kotlin)
-    implementation(libs.jackson.dataformat.yaml)
+    api(projects.egLibsRegistry)
+    api(platform(libs.jackson.bom))
+    api(libs.jackson.module.kotlin)
+    api(libs.jackson.dataformat.yaml)
     implementation(libs.yaml.config.updater)
 
     compileOnly(paperLibs.adventure.key)
