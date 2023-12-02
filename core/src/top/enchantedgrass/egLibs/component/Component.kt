51033b62eb8eb69256006bd350c48ec5d9e991a8
Tozy Fullbuster <tozyfullbuster@gmail.com>
2023-12-02 20:33:44 +0700

    feat(core): add String.mdcList replaceFirst boolean param

--- core/src/top/enchantedgrass/egLibs/component/Component.kt
+++ core/src/top/enchantedgrass/egLibs/component/Component.kt
@@ -25,22 +25,46 @@ fun String.mdc(placeholders: List<Placeholder>): Component =
     MineDown(this).replaceFirst(true).replace(placeholders.toMap()).toComponent()
 
 /**
- * Deserializes this string to a list component using [MineDown].
- * The placeholders are replaced using [placeholders].
+ * Replaces the placeholders in this string with the provided values and converts it into a list of components
+ * using the [MineDown] library.
+ *
+ * @param placeholders The list of placeholders to be replaced in the string.
+ * @param replaceFirst Indicates should replace string placeholders before splitting the string.
+ * @return The resulting list of components after replacing the placeholders.
  *
  * @see MineDown
  * @see Placeholder
  */
-fun String.mdcList(vararg placeholders: Placeholder): List<Component> = mdcList(placeholders.toList())
+fun String.mdcList(vararg placeholders: Placeholder, replaceFirst: Boolean = false): List<Component> =
+    mdcList(placeholders.toList(), replaceFirst)
 
 /**
- * Deserializes this string to a list component using [MineDown].
- * The placeholders are replaced using [placeholders].
+ * Replaces the placeholders in this string with the provided values and converts it into a list of components
+ * using the [MineDown] library.
+ *
+ * @param placeholders The list of placeholders to be replaced in the string.
+ * @param replaceFirst Indicates should replace string placeholders before splitting the string.
+ * @return The resulting list of components after replacing the placeholders.
  *
  * @see MineDown
  * @see Placeholder
  */
-fun String.mdcList(placeholders: List<Placeholder>): List<Component> = split("\n").mdc(placeholders)
+fun String.mdcList(placeholders: List<Placeholder>, replaceFirst: Boolean = false): List<Component> {
+    var result = this
+    val remainingPlaceholders = placeholders.toMutableList()
+    if (replaceFirst) {
+        for (placeholder in placeholders) {
+            val (key, value) = placeholder
+            if (value !is String) {
+                continue
+            }
+
+            result = result.replace(key, value)
+            remainingPlaceholders -= placeholder
+        }
+    }
+    return result.split("\n").mdc(remainingPlaceholders)
+}
 
 /**
  * Deserializes this list of strings to a list of components using [MineDown].
